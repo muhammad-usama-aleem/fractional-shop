b192d9946a5edbb9bf52e52c763150c7e718e4d8
muhammad-usama-aleem <you@example.com>
2020-12-31 17:24:34 +0500

    Delete all the code files because of repetetion

--- src/component/CardDeatilsFooter/CardDetailsFooter.jsx
+++
@@ -1,1 +0,0 @@
-CardDetailsFooter.jsx
--- src/styles/theme/colors.js
+++
@@ -1,9 +0,0 @@
-export const googleWhite = '#ffffff';
-export const googleBlue = ' #4285F4ff';
-export const colorPrimary = '#2C5BC8';
-export const silverWhite = '#F8F9FC';
-export const darkGray = '#707070';
-export const eerieBlack = '#1F1F1F';
-export const oceanGreen = '#54B98A';
-export const imperialRed = '#E24848';
-export const yellowOrange = '#FFAD3C';
